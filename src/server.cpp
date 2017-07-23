73051a382cc1ddd759ee8d8331fd55a598d85856
darwinyip <yipdarwin@gmail.com>
2017-07-23 19:49:21 -0400

    Refactor main parts into a HttpServer class

--- src/server.cpp
+++ src/server.cpp
@@ -6,36 +6,66 @@
 #include <unistd.h>
 #include <errno.h>
 #include <netdb.h>
+#include <stdexcept>
 
-int main(int argc, char const *argv[])
-{
+class HttpServer {
+public:
+	HttpServer();
+	~HttpServer();
+	bool start(int port);
+	int getServerSocket();
+
+private:
+	int server_socket_fd;
+};
+
+HttpServer::HttpServer() {}
+
+HttpServer::~HttpServer() {}
+
+bool HttpServer::start(int port) {
 	struct addrinfo hints, *res;
 	std::memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = INADDR_ANY;
 
-	getaddrinfo(NULL, "8888", &hints, &res);
+	getaddrinfo(NULL, std::to_string(port).c_str(), &hints, &res);
 
-	int server_socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	this->server_socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-	if(server_socket_fd == -1) {
-		printf("Socket error: %s\n", strerror(errno));
+	if(this->server_socket_fd == -1) {
+		std::cerr << "Socket error: " << strerror(errno);
+		return EXIT_FAILURE;
 	}
 
-	if(bind(server_socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
-		printf("Binding error: %s\n", strerror(errno));
+	if(bind(this->server_socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
+		std::cerr << "Binding error: " << strerror(errno);
+		return EXIT_FAILURE;
 	}
 
-	if(listen(server_socket_fd, 10) == -1) {
-		printf("Listen error: %s\n", strerror(errno));
+	if(listen(this->server_socket_fd, 10) == -1) {
+		std::cerr << "Listening error: " << strerror(errno);
+		return EXIT_FAILURE;
 	}
+	return EXIT_SUCCESS;
+}
+
+int HttpServer::getServerSocket() {
+	return this->server_socket_fd;
+}
+
+int main(int argc, char const *argv[])
+{
+
+	HttpServer server;
+	server.start(8888);
 
 	struct sockaddr_storage incoming_addr;
 
 
 	socklen_t incoming_addr_size = sizeof(incoming_addr);
-	int incoming_fd = accept(server_socket_fd, (struct sockaddr*) &incoming_addr, &incoming_addr_size);
+	int incoming_fd = accept(server.getServerSocket(), (struct sockaddr*) &incoming_addr, &incoming_addr_size);
 
 	if(incoming_fd == -1) {
 		printf("Accept error: %s\n", strerror(errno));
